d247a9e77d7f5cc893a112fff85cd0c8e894e7d8
Ivan <ilchenkoivan92@gmail.com>
2020-07-09 21:47:52 +0300

    well work

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function () {
     $(".hamburger").click(function () {
         $(this).toggleClass("is-active");
+        $(".navigation-menu").toggleClass("open-menu");
     });
+
+    $(".navigation-menu__link").click(function () {
+        $(".hamburger").removeClass("is-active");
+        $(".navigation-menu").removeClass("open-menu");
+    });
+
+
+    // On scroll menu fixed
+
+    $(window).scroll(() => {
+        var windowTop = $(window).scrollTop();
+        windowTop > 30 ? $('.navigation').addClass('scroll-fixed') : $('.navigation').removeClass('scroll-fixed');
+    });
+
+    // код для плавної прокрутки
+
+    $('.anchor[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 200);
+
+    });
+
+
 });
